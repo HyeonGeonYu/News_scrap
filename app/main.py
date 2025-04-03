9644b75b4be0efeaa644503ef65638df4cb2fde8
HyeongeonYu <yuhyeongeon@korea.ac.kr>
2025-04-03 18:24:45 +0900

    edit frontend

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from .URL찾기 import get_latest_video_url
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# CORS 설정 추가
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://my-frontend-3txviy6ms-hyeongeons-projects.vercel.app"],
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 HTTP 메서드 허용 (GET, POST 등)
+    allow_headers=["*"],  # 모든 헤더 허용
+)
 
 @app.get("/youtube")
 async def get_youtube():
