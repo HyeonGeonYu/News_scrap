5f8036760248a2c393c8e35cb0f346000c7df101
HyeongeonYu <yuhyeongeon@korea.ac.kr>
2025-04-04 12:54:49 +0900

    edit frontend

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 # CORS 설정 추가
 app.add_middleware(
     CORSMiddleware,
-        allow_origins=["https://my-frontend-delta.vercel.app"],
+    allow_origins=["*"],  # 모든 도메인 허용 (보안 문제 있으면 특정 도메인만 허용)
     allow_credentials=True,
     allow_methods=["*"],  # 모든 HTTP 메서드 허용 (GET, POST 등)
     allow_headers=["*"],  # 모든 헤더 허용
