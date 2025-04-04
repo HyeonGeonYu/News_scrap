87b5315488182c839fb81e4793e9cd1df2a66aa8
Lion noiL <108247128+lion-noil@users.noreply.github.com>
2025-04-04 12:40:14 +0900

    edit frontend

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 # CORS 설정 추가
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://my-frontend-3txviy6ms-hyeongeons-projects.vercel.app"],
+        allow_origins=["https://my-frontend-delta.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],  # 모든 HTTP 메서드 허용 (GET, POST 등)
     allow_headers=["*"],  # 모든 헤더 허용
