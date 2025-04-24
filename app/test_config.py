9a297087723e42a7f78b9eb480db3611bcd47803
HyeongeonYu <yuhyeongeon@korea.ac.kr>
2025-04-24 16:23:27 +0900

    edit bug youtube update

--- app/test_config.py
+++ app/test_config.py
@@ -1,23 +1,5 @@
 channels = [
-        {"country": "Korea",
-         "channel_handle": "@newskbs",
-         "playlist_id": ["PL9a4x_yPK_85sGRvAQX4LEVHY8F9v405J","PL9a4x_yPK_84yhk67LjymVv7kbI5eyOVU"],
-         "keyword": ["[풀영상] 뉴스12", "[풀영상] 뉴스광장"],
-         "save_fields": "subtitle"},
-        {
-            "country": "USA",
-            "channel_handle": "@NBCNews",
-            "playlist_id": "PL0tDb4jw6kPymVj5xNNha5PezudD5Qw9L",
-            "keyword": "Nightly News Full Episode",
-            "save_fields": "subtitle"
-        },
-        {
-            "country": "Japan",
-            "channel_handle": "@tbsnewsdig",
-            "keyword": "【LIVE】朝のニュース（Japan News Digest Live）",
-            "playlist_id": "PLhoNlZaJqDLaPgn1NqC9FxMPnlkemRpyr",
-            "save_fields": "subtitle"
-        },
+
         {
             "country": "China",
             "channel_handle": "@CCTV",
